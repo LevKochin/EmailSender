cae892db8d7bd0520c6ee9db31a5a7d70055a838
Lev Kochin <rat.ratio@yandex.ru>
2021-02-09 13:32:04 +0600

    Add project files.

---
+++ EmailSender/EmailSenderDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmailSender
+{
+    public partial class EmailSenderDbContext : DbContext
+    {
+        public EmailSenderDbContext()
+        {
+        }
+
+        public EmailSenderDbContext(DbContextOptions<EmailSenderDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EmailSender.Db;Trusted_Connection=True;");
+            }
+        }
+    }
+}
---
+++ EmailSender/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mailRecipients = new List<MailAddress>()
+            {
+                new MailAddress("somemail@gmail.com"),
+                new MailAddress("anymail@gmail.com"),
+                new MailAddress("someKindOfmail@gmail.com"),
+                new MailAddress("rat.ratio@yandex.ru"),
+            };
+            var mailFrom = new MailAddress("rat.ratio888@gmail.com", "Lev");
+            var smtp = new SmtpClient("smtp.gmail.com", 587);
+            var subjectMessage = "Sending data from the database";
+            Console.WriteLine($"{subjectMessage}");
+            Console.WriteLine($"Departure to - {smtp.Host}");
+            Console.WriteLine($"Sender: name - {mailFrom.Address}, address - {mailFrom.DisplayName}");
+            using (var db = new EmailSenderDbContext())
+            {
+                var users = db.Users.ToList();
+                Console.WriteLine("Generating a mail for users:");
+                foreach (var mailTo in mailRecipients)
+                {
+                    Console.WriteLine($"Address - {mailTo.Address}");
+                    var mail = new MailMessage(mailFrom, mailTo);
+                    mail.Subject = subjectMessage;
+                    mail.IsBodyHtml = true;
+                    smtp.Credentials = new NetworkCredential(mailFrom.Address, "3822557455Leo");
+                    foreach (var user in users)
+                    {
+                        var message = $"<p> Name - {user.Name} </p>";
+                        mail.Body += message;
+                    }
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+            Console.WriteLine("Messages have been sent!");
+            Console.ReadKey();
+        }
+    }
+}
